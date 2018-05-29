5da771bb53c5a2bb06b213bd8377749bef4860d2
Ronnie <ron.laws86@gmail.com>
2018-05-29 12:14:26 +0100

    Update custom.js
    Code Cleanup and split columns in to separate timer threads.

--- www/html/assets/js/custom.js
+++ www/html/assets/js/custom.js
@@ -8,37 +8,51 @@ audio["alarm"].play();
 });
   // audio["alarm"].play();
 
-var timeout = setInterval(pingHosts, 5000);
+var colSite1 = setInterval(pingHosts, 5000);
 function pingHosts () {
 	//Site 1
 	$('#11').load('host.php?ip=8.8.8.8&name=Google%20DNS');
 	$('#12').load('host.php?ip=#&name=#');
 	$('#13').load('host.php?ip=#&name=#');
 	$('#14').load('host.php?ip=#&name=#');
-
+}
+var colSite2 = setInterval(pingHosts, 5000);
+function pingHosts () {
 	//Site 2
 	$('#21').load('host.php?ip=8.8.4.4&name=Google%20DNS');
 	$('#22').load('host.php?ip=#&name=#');
 	$('#23').load('host.php?ip=#&name=#');
 	$('#24').load('host.php?ip=#&name=#');
+}
 
+var colSite3 = setInterval(pingHosts, 5000);
+function pingHosts () {
 	//Site 3
 	$('#31').load('host.php?ip=#&name=#');
 	$('#32').load('host.php?ip=#&name=#');
 	$('#33').load('host.php?ip=#&name=#');
+	$('#34').load('host.php?ip=#&name=#');
+}
 
+var colSite4 = setInterval(pingHosts, 5000);
+function pingHosts () {
 	//Site 4
 	$('#41').load('host.php?ip=#&name=#');
 	$('#42').load('host.php?ip=#&name=#');
-	$('#43').load('host.php?ip=#&name=#');;
+	$('#43').load('host.php?ip=#&name=#');
+	$('#44').load('host.php?ip=#&name=#');
+}
 
+var colSite5 = setInterval(pingHosts, 5000);
+function pingHosts () {
 	//Site 5 (large)
 
 	//col 1
-	$('#51a').load('host.php?ip=#&name=#');;
+	$('#51a').load('host.php?ip=#&name=#');
 	$('#52a').load('host.php?ip=#&name=#');
 	$('#53a').load('host.php?ip=#&name=#');
 	$('#54a').load('host.php?ip=#&name=#');
+	$('#55a').load('host.php?ip=#&name=#');
 
 	//col2
 	$('#51b').load('host.php?ip=#&name=#');
@@ -53,7 +67,6 @@ function pingHosts () {
 	$('#53c').load('host.php?ip=#&name=#');
 	$('#54c').load('host.php?ip=#&name=#');
 	$('#55c').load('host.php?ip=#&name=#');
-	$('#56c').load('host.php?ip=#&name=#');
 
 	//col 4
 	$('#51d').load('host.php?ip=#&name=#');
